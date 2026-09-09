8f10f61fb7869631b2cae3441cbcbd18abce2004
PrismaticFlame <chrisw15029@gmail.com>
2026-09-08 22:58:55 -0400

    comments on daily 3870

--- Python/daily_3870.py
+++ Python/daily_3870.py
@@ -1,4 +1,5 @@
 import resource
+import tracemalloc
 
 class Solution:
     def countCommas(self, n: int) -> int:
@@ -17,5 +18,15 @@ class Solution2:
         return len(str(n)) // 3 + n - 1000
 
 if __name__ == "__main__":
+    tracemalloc.start()
     print(Solution().countCommas(1002))
+    current, peak = tracemalloc.get_traced_memory()
+    print(f"current={current}, peak={peak} bytes")
+    tracemalloc.stop()
     print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss, "KB")
+
+    # was getting annoyed with leetcode saying my solution, Solution, was using more memory
+    # than another persons solution, Solution2, and wanted to see how much memory was being used.
+    # mine overall uses less memory, but leetcode seems to think otherwise
+    # AI says small problems have variable memory usages due to Python overhead, initialization, and 
+    # leet code judge harness have some noise going on
